5d5ef57d5fbad5505d840cd3110ac6b14d13c976
alex-lbv <aal940722@gmail.com>
2021-05-13 16:43:53 +0300

    Добавил модальное окно, поправил баги

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,6 +3,9 @@ const menuButton = header.querySelector('.header__menu');
 const menu = header.querySelector('.header__navigation');
 const title = document.querySelector('h1');
 const button = document.querySelector('.btn');
+const services = document.querySelectorAll('.services__card');
+const promotions = document.querySelectorAll('.promotions__item');
+const costButton = document.querySelector('.first-screen__container .btn');
 
 const windowScroll = () => {
   const state = window.pageYOffset;
@@ -19,10 +22,19 @@ const titleActive = () => {
   title.classList.toggle('active');
 };
 
+const mobileServices = () => {
+  for (const service of services) {
+    const topCoords = service.getBoundingClientRect().top;
+    topCoords < 300 && topCoords > 0 ? service.classList.add('opened') : service.classList.remove('opened');
+  }
+};
 
-
-menuButton.addEventListener('click', openMenu);
-window.addEventListener('scroll', windowScroll);
+const mobilePromotions = () => {
+  for (const promotion of promotions) {
+    const topCoords = promotion.getBoundingClientRect().top;
+    topCoords < 300 && topCoords > 0 ? promotion.classList.add('opened') : promotion.classList.remove('opened');
+  }
+};
 
 const swiperReview = new Swiper(".review-swiper", {
   navigation: {
@@ -70,7 +82,47 @@ const swiperGallery = new Swiper(".gallery-swiper", {
   },
 });
 
-button.addEventListener('mouseover', titleActive);
+menuButton.addEventListener('click', openMenu);
+window.addEventListener('scroll', windowScroll);
 
+if (window.innerWidth < 1099) {
+  document.addEventListener('scroll', mobileServices);
+  document.addEventListener('scroll', mobilePromotions);
+} else {
+  document.removeEventListener('scroll', mobileServices);
+  document.removeEventListener('scroll', mobilePromotions)
+}
+
+const isEscEvent = (evt) => {
+  return evt.key === ('Escape' || 'Esc');
+};
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscEvent(evt)) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+const openPopup = () => {
+  cost.classList.add('opened');
+  document.body.classList.add('hidden')
+  document.addEventListener('keydown', onPopupEscKeydown);
+};
+
+const closePopup = () => {
+  cost.classList.remove('opened');
+  document.body.classList.remove('hidden')
+  document.removeEventListener('keydown', onPopupEscKeydown);
+};
+
+button.addEventListener('mouseover', titleActive);
 button.addEventListener('mouseout', titleActive);
 
+const cost = document.querySelector('.cost');
+const costBtnClose = cost.querySelector('.cost__close');
+const costBg = cost.querySelector('.cost__bg');
+
+costButton.addEventListener('click', openPopup);
+costBg.addEventListener('click', closePopup);
+costBtnClose.addEventListener('click', closePopup);
