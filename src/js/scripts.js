b3295f7e94fa598c2a884c8965eeec941d8a5586
alex-lbv <aal940722@gmail.com>
2021-06-09 12:10:55 +0300

    Вносит правки в css

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -92,10 +92,6 @@ if (window.innerWidth < 1099) {
   document.addEventListener('scroll', mobileServices);
   document.addEventListener('scroll', mobilePromotions);
 }
-/* else {
-  document.removeEventListener('scroll', mobileServices);
-  document.removeEventListener('scroll', mobilePromotions)
-}*/
 
 const isEscEvent = (evt) => {
   return evt.key === ('Escape' || 'Esc');
@@ -130,3 +126,49 @@ const costBg = cost.querySelector('.cost__bg');
 costButton.addEventListener('click', openPopup);
 costBg.addEventListener('click', closePopup);
 costBtnClose.addEventListener('click', closePopup);
+
+const maskPhone =(selector, masked = '+7 (___) ___-__-__') => {
+  const elements = document.querySelectorAll(selector);
+
+  function mask(event) {
+    const keyCode = event.keyCode;
+    const template = masked,
+      def = template.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, "");
+    let i = 0,
+      newValue = template.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+      });
+    i = newValue.indexOf("_");
+    if (i !== -1) {
+      newValue = newValue.slice(0, i);
+    }
+    let reg = template.substr(0, this.value.length).replace(/_+/g,
+      function (a) {
+        return "\\d{1," + a.length + "}";
+      }).replace(/[+()]/g, "\\$&");
+    reg = new RegExp("^" + reg + "$");
+    if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+      this.value = newValue;
+    }
+    if (event.type === "blur" && this.value.length < 5) {
+      this.value = "";
+    }
+
+  }
+
+  for (const elem of elements) {
+    elem.addEventListener("input", mask);
+    elem.addEventListener("focus", mask);
+    elem.addEventListener("blur", mask);
+  }
+
+}
+
+maskPhone('input[type="tel"]');
+
+const menuItem = document.querySelector('#menu-item-214');
+
+if (menuItem && window.innerWidth < 1099) {
+  menuItem.addEventListener('click', openMenu);
+}
